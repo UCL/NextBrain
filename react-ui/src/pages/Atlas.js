f9fd4e242fc5138ccf4707c631fd9f554ba1153c
James Hughes <james.hughes@ucl.ac.uk>
2021-04-16 17:30:34 +0100

    added skeleton for all main atlas panels

--- react-ui/src/pages/Atlas.js
+++ react-ui/src/pages/Atlas.js
@@ -4,18 +4,18 @@ const Atlas = () => {
 	return (
 		<main className="atlas-container">
 			<section className="panels-container">
-				<div className="side-panel-1"></div>
-				<div className="side-panel-2"></div>
-				<div className="side-panel-3"></div>
-				<div className="main-panel"></div>
+				<div className="side-panel-1">Sagittal panel</div>
+				<div className="side-panel-2">Coronal panel</div>
+				<div className="side-panel-3">Axial panel</div>
+				<div className="main-panel">Histology panel</div>
 				<div className="scrollbar"></div>
 			</section>
 			<section className="options-container">
-				<div className="atlas-navigation"></div>
-				<div className="show-labels"></div>
-				<div className="label-transparency"></div>
-				<div className="chennel"></div>
-				<div className="current-label"></div>
+				<div className="atlas-navigation">Atlas navigation</div>
+				<div className="show-labels"> Show labels</div>
+				<div className="label-transparency">Label transparency</div>
+				<div className="channel">Channel</div>
+				<div className="current-label"> Current label</div>
 			</section>
 		</main>
 	);
