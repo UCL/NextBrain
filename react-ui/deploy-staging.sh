e713cdd17ce717f7bad4f9c4a83e42a0d182e824
James Hughes <james.hughes@ucl.ac.uk>
2021-08-06 16:06:34 +0100

    added shell script for deploying to staging

---
+++ react-ui/deploy-staging.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "deploying to staging"
+rm -rf build
+PUBLIC_URL=https://jhughes982.github.io/brain-atlas-staging/ npm run build
+cd build
+git init
+git add .
+git commit -m "staging deployment"
+git remote add origin https://github.com/jhughes982/brain-atlas-staging.git
+git push --force origin master:gh-pages
