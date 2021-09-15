dc5973d9d164f6140e52d1977e59c42ff14a1120
James Hughes <james.hughes@ucl.ac.uk>
2021-09-15 17:06:40 +0100

    added prototype for atlas navigation (using library)

--- react-ui/src/components/atlas/atlasOptions/AtlasNavigation.tsx
+++ react-ui/src/components/atlas/atlasOptions/AtlasNavigation.tsx
@@ -1,7 +1,42 @@
 import { FC } from "react";
 
+import DropdownTreeSelect from "react-dropdown-tree-select";
+import "react-dropdown-tree-select/dist/styles.css";
+
 const AtlasNavigation: FC = () => {
-	return <div>Atlas navigation</div>;
+	const data = {
+		label: "Left Cerebral White Matter",
+		value: "lcwm",
+		children: [
+			{
+				label: "Frontomarginal gyrus",
+				value: "fmg",
+			},
+			{
+				label: "White matter of forebrain",
+				value: "wmof",
+			},
+		],
+	};
+
+	const onChange = (currentNode: any, selectedNodes: any) => {
+		console.log("onChange::", currentNode, selectedNodes);
+	};
+	const onAction = (node: any, action: any) => {
+		console.log("onAction::", action, node);
+	};
+	const onNodeToggle = (currentNode: any) => {
+		console.log("onNodeToggle::", currentNode);
+	};
+
+	return (
+		<DropdownTreeSelect
+			data={data}
+			onChange={onChange}
+			onAction={onAction}
+			onNodeToggle={onNodeToggle}
+		/>
+	);
 };
 
 export default AtlasNavigation;
