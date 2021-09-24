0ce4e4204168adcf6effbe93ada9a15661911aa2
James Hughes <james.hughes@ucl.ac.uk>
2021-09-24 18:08:34 +0100

    removing hi res images (to be replaced with lower quality versions)

--- react-ui/deploy-staging.sh
+++ react-ui/deploy-staging.sh
@@ -7,6 +7,6 @@ cd build
 git init
 git add .
 git commit -m "staging deployment"
-git remote add origin https://github.com/jhughes982/brain-atlas-staging.git
-#git remote add origin git@github.com:jhughes982/brain-atlas-staging.git
+#git remote add origin https://github.com/jhughes982/brain-atlas-staging.git
+git remote add origin git@github.com:jhughes982/brain-atlas-staging.git
 git push --force origin master:gh-pages
