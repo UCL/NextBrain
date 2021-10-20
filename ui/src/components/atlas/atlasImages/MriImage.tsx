a1086558d8c18106919ade865f235ab460474b0c
James Hughes <james.hughes@ucl.ac.uk>
2021-10-20 19:30:22 +0100

    added a loading spinner while mri images are fetched from remote github repo

--- ui/src/components/atlas/atlasImages/MriImage.tsx
+++ ui/src/components/atlas/atlasImages/MriImage.tsx
@@ -2,6 +2,7 @@ import { FC, useEffect, useState } from "react";
 
 import { ASSETS_URL } from "../../utils/ASSETS_URL";
 import ErrorModal from "../../shared/ErrorModal";
+import LoadingSpinner from "../../shared/LoadingSpinner";
 import MousePointer from "../../shared/MousePointer";
 
 import { MriCoords } from "../../../models/mriCoords.model";
@@ -19,7 +20,8 @@ interface Props {
 }
 
 const MriImage: FC<Props> = (props) => {
-	const [error, setError] = useState<string | null>();
+	const [error, setError] = useState<string | null>(null);
+	const [isLoading, setIsLoading] = useState(false);
 	const [mriImage, setMriImage] = useState("");
 	const [currentSlice, setCurrentSlice] = useState("");
 
@@ -32,27 +34,43 @@ const MriImage: FC<Props> = (props) => {
 			.toFixed(0)
 			.padStart(3, "0");
 
-		try {
-			const mriImage = `${ASSETS_URL}${patientId}/mri_rotated/slices_${plane}/slice_${paddedSlice}.png`;
+		setCurrentSlice(paddedSlice);
 
-			setMriImage(mriImage);
-			setCurrentSlice(paddedSlice);
-		} catch (e) {
-			console.log(
-				`%cerror, could not load mri image, ${e},
-				"color: red"`
-			);
-		}
+		const getMriImage = async () => {
+			try {
+				setIsLoading(true);
+				const mriImageUrl = `${ASSETS_URL}${patientId}/mri_rotated/slices_${plane}/slice_${paddedSlice}.png`;
+
+				// fetch the image from the server so we can utilise await and show a loading spinner
+				// in reality we don't use the fetched data, we use the raw url in mriImageUrl
+				const response = await fetch(mriImageUrl);
+
+				if (response.ok) {
+					setMriImage(mriImageUrl);
+				}
+			} catch (e) {
+				setError("Error, could not load mri image");
+				console.log(e);
+			}
+			setIsLoading(false);
+		};
+
+		getMriImage();
 	}, [mriImageCoords, plane, patientId]);
 
-	if (mriImage === null || currentSlice === null) {
-		return <div>Could not build mri image</div>;
+	if (mriImage == null || mriImage === "" || currentSlice == null) {
+		return (
+			<>
+				<ErrorModal error={error} onClear={() => setError(null)} />
+				{isLoading && <LoadingSpinner asOverlay />}
+				{!isLoading && <div>Failed to load mri image</div>}
+			</>
+		);
 	}
 
 	return (
 		<>
-			<ErrorModal error={error!} onClear={() => setError(null)} />
-
+			<ErrorModal error={error} onClear={() => setError(null)} />
 			<div className={`mri-img ${plane}`}>
 				<div className={`mri-img-container ${plane}`}>
 					<MousePointer
--- ui/src/components/shared/LoadingSpinner.tsx
+++ ui/src/components/shared/LoadingSpinner.tsx
@@ -10,6 +10,7 @@ const LoadingSpinner: FC<Props> = (props) => {
 	return (
 		<div className={`${props.asOverlay && "loading-spinner__overlay"}`}>
 			<div className="lds-dual-ring"></div>
+			<div>Loading...</div>
 		</div>
 	);
 };
