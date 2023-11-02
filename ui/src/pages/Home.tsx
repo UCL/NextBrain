da311a02668e88f2116ade4663f5a08bd8d55efd
Adrià Casamitjana <acasamitjana91@gmail.com>
2023-11-02 14:20:40 +0100

    edit homepage

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -4,6 +4,7 @@ import banner from "../assets/homepage-banner.png";
 import uclLogo from "../assets/ucl-logo.png";
 import ercLogo from "../assets/erc-logo.png";
 import euLogo from "../assets/eu-logo.png";
+import pipelineImage from "../assets/pipeline-image.png";
 
 import "./Home.css";
 
@@ -32,53 +33,112 @@ const Atlas: FC = () => {
 					A comprehensive description of the 3D probabilistic atlas and its application
 					to brain Bayesian segmentation can be found in the following publication:
 				</p>
-				
+
 				<cite>
 					<a>
-						[In preparation] "BLUE-Brain: a next generation probabilistic atlas 
+						<b> [In preparation] "BLUE-Brain: a next generation probabilistic atlas
 						of human brain anatomy built from 3D histology and companion 
-						Bayesian segmentation tool" 
+						Bayesian segmentation tool"</b>
 					</a>
 				</cite>
 
-				
-				<p>
-					The raw data of the project can be found here: doi.org/10.5522/04/24243835 
-					The data acquisition and tissue processing pipeline are described in the 
-					following publication:
-				</p>
+                <br />
+                <br />
+                <br />
+				<img className="pipeline-banner" src={pipelineImage} alt="3D reconstruction pipeline"></img>
 
-				<cite>
-					<a
-						href="https://www.nature.com/articles/s41598-020-69163-z"
-						target="_blank"
-						rel="noopener noreferrer"
-						className="reference"
-					>
-						Mancini, Matteo, Adrià Casamitjana, Loic Peter, Eleanor Robinson,
-						Shauna Crampsie, David L. Thomas, Janice L. Holton, Zane
-						Jaunmuktane, and Juan Eugenio Iglesias. "A multimodal computational
-						pipeline for 3D histology of the human brain." Scientific reports.
-					</a>
-				</cite>
 
-				<p>
-					The histological sections are registered to the coordinate space of the
-					ex vivo MRI using a pipeline described in the following publications:
-				</p>
-				
-				<cite>
-					<a
-						href="https://www.sciencedirect.com/science/article/pii/S1361841521003108"
-						target="_blank"
-						rel="noopener noreferrer"
-						className="reference"
-					>
-						Casamitjana, Adrià, et al. "Robust joint registration of multiple 
-						stains and MRI for multimodal 3D histology reconstruction: Application 
-						to the Allen human brain atlas." Medical image analysis 75 (2022): 102265.
-					</a>
-				</cite>
+
+				<section>
+                <h3>Other publications</h3>
+                 <ul>
+                  <li>
+                    <p>
+                        The raw data of the project can be found here:
+                    </p>
+                    <cite>
+                        <a
+                            href="doi.org/10.5522/04/24243835"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="reference"
+                        >
+                            Mancini, Matteo, Adrià Casamitjana, Loic Peter, Eleanor Robinson,
+                            Shauna Crampsie, David L. Thomas, Janice L. Holton, Zane
+                            Jaunmuktane, and Juan Eugenio Iglesias. "A multimodal computational
+                            pipeline for 3D histology of the human brain." Scientific reports.
+                        </a>
+                    </cite>
+
+                  </li>
+                  <li>
+                    <p>
+                        The data acquisition and tissue processing pipeline are described in the
+                        following publication:
+                    </p>
+
+                    <cite>
+                        <a
+                            href="https://www.nature.com/articles/s41598-020-69163-z"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="reference"
+                        >
+                            Mancini, Matteo, Adrià Casamitjana, Loic Peter, Eleanor Robinson,
+                            Shauna Crampsie, David L. Thomas, Janice L. Holton, Zane
+                            Jaunmuktane, and Juan Eugenio Iglesias. "A multimodal computational
+                            pipeline for 3D histology of the human brain." Scientific reports.
+                        </a>
+                    </cite>
+                  </li>
+                  <li>
+                    <p>
+                        The histological sections are registered to the coordinate space of the
+                        ex vivo MRI using a pipeline described in the following publications:
+                    </p>
+
+                    <cite>
+                        <a
+                            href="https://www.sciencedirect.com/science/article/pii/S1361841521003108"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="reference"
+                        >
+                            Casamitjana, Adrià, et al. "Robust joint registration of multiple
+                            stains and MRI for multimodal 3D histology reconstruction: Application
+                            to the Allen human brain atlas." Medical image analysis 75 (2022): 102265.
+                        </a>
+                    </cite>
+                  </li>
+                </ul>
+
+                </section>
+
+                 <section>
+                <h3>Authors</h3>
+                <ul>
+                    <li><p><b>James Hughes</b><br />
+                    [<a title="" href="https://github.com/jhughes982" target="_blank" rel="noopener">Github</a>]</p>
+                    </li>
+
+                    <li><p><b> Peter Schmidt</b><br />
+                    [<a title="" href="https://github.com/pweschmidt" target="_blank" rel="noopener">Github</a>]</p>
+                    </li>
+
+                    <li><p><b>Adria Casamitjana</b><br />
+                    a.casamitjana [at] ub.edu<br />
+                    [<a title="" href="https://twitter.com/JuanEugenioIgl1" target="_blank" rel="noopener">Twitter</a>  |  
+                    <a title="" href="https://scholar.google.com/citations?user=phHLLH0AAAAJ&hl=ca&oi=ao" target="_blank" rel="noopener">Scholar</a>]</p>
+                    </li>
+
+                    <li><p><b>Juan Eugenio Iglesias</b><br />
+                    jiglesiasgonzalez [at] mgh.harvard.edu<br />
+                    [<a title="" href="https://twitter.com/therelaxationt1" target="_blank" rel="noopener">Twitter</a>  |  
+                    <a title="" href="https://www.linkedin.com/in/juan-eugenio-iglesias-820565127/" target="_blank" rel="noopener">Linkedin</a>  |  
+                    <a title="" href="https://scholar.google.com/citations?user=_f2iSSQAAAAJ" target="_blank" rel="noopener">Scholar</a>]</p>
+                    </li>
+                </ul>
+                </section>
 
 				<p>
 					This research is primarily funded by the European Research Council{" "}
