e3f7948c920b7f205e525465d8aa9916c2294b1a
Adrià Casamitjana <a.casamitjana@ucl.ac.uk>
2023-11-02 11:22:16 +0100

    Update Home.tsx

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -14,23 +14,38 @@ const Atlas: FC = () => {
 				<img className="header-banner" src={banner} alt="logo"></img>
 
 				<h1 className="homepage-title">
-					A 3D histological atlas of the human brain for high-resolution
+					BLUE-Brain: a 3D histological atlas of the human brain for high-resolution
 					neuroimaging studies
 				</h1>
 			</div>
 
 			<div className="homepage-body">
 				<p>
-					We are building a new atlas of the human brain using histological
-					sections of five full human hemispheres with manual annotations for
-					~300 structures. This website enables the interactive inspection of
-					one of these cases using a 3D navigation interface and search
-					functionality. Further cases will be uploaded soon.
+					We are presenting a next-generation probabilistic atlas of the human 
+					brain using histological sections of five full human hemispheres 
+					with manual annotations for ~300 structures. This website enables the
+					interactive inspection of these five cases using a 3D navigation 
+					interface and search functionality.
 				</p>
 
 				<p>
-					The histological sections are registered to the coordinate space of an
-					ex vivo MRI using a pipeline described in the following publication:
+					A comprehensive description of the 3D probabilistic atlas and its application
+					to brain Bayesian segmentation can be found in the following publication:
+				</p>
+				
+				<cite>
+					<a>
+						[In preparation] "BLUE-Brain: a next generation probabilistic atlas 
+						of human brain anatomy built from 3D histology and companion 
+						Bayesian segmentation tool" 
+					</a>
+				</cite>
+
+				
+				<p>
+					The raw data of the project can be found here: doi.org/10.5522/04/24243835 
+					The data acquisition and tissue processing pipeline are described in the 
+					following publication:
 				</p>
 
 				<cite>
@@ -44,6 +59,23 @@ const Atlas: FC = () => {
 						Shauna Crampsie, David L. Thomas, Janice L. Holton, Zane
 						Jaunmuktane, and Juan Eugenio Iglesias. "A multimodal computational
 						pipeline for 3D histology of the human brain." Scientific reports.
+					</a>
+				</cite>
+
+				<p>
+					The histological sections are registered to the coordinate space of the
+					ex vivo MRI using a pipeline described in the following publications:
+				</p>
+				
+				<cite>
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						className="reference"
+					>
+						Casamitjana, Adrià, et al. "Robust joint registration of multiple 
+						stains and MRI for multimodal 3D histology reconstruction: Application 
+						to the Allen human brain atlas." Medical image analysis 75 (2022): 102265.
 					</a>
 				</cite>
 
